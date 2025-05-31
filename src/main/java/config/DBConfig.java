5947ee94355ccacfd79635b714cc6ead52d71ce9
Charyear <charyear@126.com>
2025-05-31 15:20:39 +0800

    Create DBConfig.java

---
+++ src/main/java/config/DBConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+public class DBConfig {
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/member_management?useSSL=false&serverTimezone=UTC";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "666666";
+} 
