2ecc1e19324306eb5cc67d453ba303bcb153287d
Charyear <charyear@126.com>
2025-05-31 15:22:56 +0800

    Create Student.java

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,89 @@
+package model;
+
+public class Student {
+    private int id;
+    private String studentId;
+    private String name;
+    private String password;
+    private String gender;
+    private int age;
+    private String major;
+    private String email;
+
+    public Student() {}
+
+    public Student(String studentId, String name, String password, String gender, int age, String major, String email) {
+        this.studentId = studentId;
+        this.name = name;
+        this.password = password;
+        this.gender = gender;
+        this.age = age;
+        this.major = major;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+} 
